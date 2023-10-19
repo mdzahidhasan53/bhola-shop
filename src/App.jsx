feebcd43251aedc7fd43fa632c52957c178f6b83
mdzahidhasan53 <mzhasan53@gmail.com>
2023-10-18 22:58:37 -0700

    set enviroment

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl text-red-500'>Bhola Shop</h1>
+      <h2>add something</h2>
+      <button className="btn btn-secondary">Secondary</button>
+      
+    </>
+  )
+}
+
+export default App
